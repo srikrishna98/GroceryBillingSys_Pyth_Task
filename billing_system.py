daa2c3ab5d4bfe943b7e85246ae8b756a339de4e
srikrishna <srikrishna0412@gmail.com>
2017-07-13 01:31:04 +0530

    Product searching and billing added

--- billing_system.py
+++ billing_system.py
@@ -1,6 +1,6 @@
 import csv
 Fn=["Name","Quantity","Cost"]
-class shop:
+class Shop:
     name=""
     qty=0
     cost=0.0
@@ -16,3 +16,39 @@ class shop:
             for row in reader :
                 print(row)
         csvf.close
+    def search_product(self,name1):
+        flag=0
+        with open("products.csv",'r')as csvf:
+            reader=csv.DictReader(csvf,fieldnames=Fn)
+            for row in reader:
+                if row["Name"] is name1:
+                    flag=1
+                    break
+            if flag==1:
+                print (name1 + " Has been found")
+                return row
+            else :
+                print (name1 + " Not found\npress 1.To add the product\t2.To search a different product\nEnter your choice: ")
+                choice=input()
+                if choice is 1:
+                    qty1=input("Enter the quantity of {} to be added".format(name1))
+                    cost1=input("Enter the cost of {} to be added".format(name1))
+                    self.add_product(name1,qty1,cost1)
+                    return row
+                elif choice is 2:
+                    name1=input("Enter a product to search:")
+                    self.search_product(name1)
+                else :return None
+    def billin(self):
+        total=0.0
+        option='C'
+        while option is not "Q":
+            self.display_product()
+            name=input("Enter the product's name")
+            row=self.search_product(name)
+            if row is not None:
+                total=total + row["Cost"]
+                row["Quantity"]=row["Quantity"]-1
+            print("product doesn't exist")
+            option=input("\npress C to continue Q to quit")
+        print("\nAmount to be payed :Rs."+total)
